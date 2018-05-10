c090e99259e0eda98d22803afbd80e62e897d10e
bfdes <bfdes@users.noreply.github.com>
2018-05-10 08:14:36 +0100

    Fix flatMap in State

--- Functional Programming in Scala/src/chapter6/State.scala
+++ Functional Programming in Scala/src/chapter6/State.scala
@@ -5,7 +5,7 @@ case class State[S, +A](run: S => (A, S)) {
   import State._
 
   def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
-    val (a, s1) = run
+    val (a, s1) = run(s)
     f(a).run(s1)
   })
 
